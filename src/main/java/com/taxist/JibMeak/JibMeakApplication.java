c3f9bc8766caee85ff3b9fd05f6e9021402e7741
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-21 12:19:20 +0200

    initialize project

---
+++ src/main/java/com/taxist/JibMeak/JibMeakApplication.java
@@ -0,0 +1,13 @@
+package com.taxist.JibMeak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibMeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibMeakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taxist/JibMeak/JibMeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxist.JibMeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibMeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
