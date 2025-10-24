26ffbb41e531977d538db21443fdfedbd7647993
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-24 16:46:20 +0200

    feat(controller):JIB-69 implement Tour controller

---
+++ src/main/java/com/taxist/JibMeak/controller/TourController.java
@@ -0,0 +1,45 @@
+package com.taxist.JibMeak.controller;
+
+import com.taxist.JibMeak.dto.TourDTO;
+import com.taxist.JibMeak.service.interfaces.TourService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+//import javax.xml.ws.Response;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tours")
+public class TourController {
+    private final TourService service;
+
+    public TourController(TourService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<TourDTO> addTour(@RequestBody TourDTO dto) {
+        TourDTO tour = service.createTour(dto);
+        return new ResponseEntity<>(tour, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TourDTO>> getAllTours() {
+        List<TourDTO> tours = service.getAllTours();
+        return ResponseEntity.ok(tours);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<TourDTO>> getTourById(@RequestParam Long id) {
+        Optional<TourDTO> tour = Optional.ofNullable(service.getTourById(id));
+        return ResponseEntity.ok(tour);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<TourDTO> deleteTourById(@RequestParam Long id) {
+        service.deleteTour(id);
+        return ResponseEntity.noContent().build();
+    }
+}
