5b44886b4bf38f2601db6f437bd0907ab7e72021
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-24 16:02:19 +0200

    feat(dto):JIB-44 Implemented Warehouse class dto

---
+++ src/main/java/com/taxist/JibMeak/dto/WarehouseDTO.java
@@ -0,0 +1,30 @@
+package com.taxist.JibMeak.dto;
+
+import java.time.LocalTime;
+
+public class WarehouseDTO {
+    private Long id;
+    private String address;
+    private double latitude;
+    private double longitude;
+    private LocalTime openHour;
+    private LocalTime closeHour;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public double getLatitude() { return latitude; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+
+    public double getLongitude() { return longitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
+
+    public LocalTime getOpenHour() { return openHour; }
+    public void setOpenHour(LocalTime openHour) { this.openHour = openHour; }
+
+    public LocalTime getCloseHour() { return closeHour; }
+    public void setCloseHour(LocalTime closeHour) { this.closeHour = closeHour; }
+}
