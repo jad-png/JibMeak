13d92b87cafd7b0c677f67f546d43eabf8047775
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-24 16:47:38 +0200

    feat(main): add main class the entry point of the app

--- src/main/java/com/taxist/JibMeak/JibMeakApplication.java
+++ src/main/java/com/taxist/JibMeak/JibMeakApplication.java
@@ -2,12 +2,14 @@ package com.taxist.JibMeak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 @SpringBootApplication
+@ImportResource("classpath:applicationContext.xml")
 public class JibMeakApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(JibMeakApplication.class, args);
+        SpringApplication.run(JibMeakApplication.class, args);
 	}
-
 }
