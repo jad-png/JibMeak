62c498043ebfe2f0894d6efefd84a277fc6a7283
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-24 15:53:30 +0200

    feat(models, wh):JIB-39 Create Warehouse entity

---
+++ src/main/java/com/taxist/JibMeak/model/Warehouse.java
@@ -0,0 +1,69 @@
+package com.taxist.JibMeak.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "warehouses")
+public class Warehouse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String address;
+
+    private double latitude;
+    private double longitude;
+
+    private LocalTime openHour;
+    private LocalTime closeHour;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public LocalTime getOpenHour() {
+        return openHour;
+    }
+
+    public void setOpenHour(LocalTime openHour) {
+        this.openHour = openHour;
+    }
+
+    public LocalTime getCloseHour() {
+        return closeHour;
+    }
+
+    public void setCloseHour(LocalTime closeHour) {
+        this.closeHour = closeHour;
+    }
+}
