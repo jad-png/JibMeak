0df685220ddfda8788dbcd939dd664168550da9b
jad-png <j.belassiria3993@uca.ac.ma>
2025-10-28 16:24:21 +0100

    untracked changes

--- src/main/java/com/taxist/JibMeak/mapper/WarehouseMapper.java
+++ src/main/java/com/taxist/JibMeak/mapper/WarehouseMapper.java
@@ -3,12 +3,11 @@ package com.taxist.JibMeak.mapper;
 import com.taxist.JibMeak.dto.WarehouseDTO;
 import com.taxist.JibMeak.model.Warehouse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface WarehouseMapper {
-
-    WarehouseMapper INSTANCE = Mappers.getMapper(WarehouseMapper.class);
 
     WarehouseDTO toDTO(Warehouse warehouse);
 
